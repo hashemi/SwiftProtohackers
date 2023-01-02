9c08e2d37a6e73378b5a6aa06bee8f3a16410dc3
Ahmad Alhashemi <ahmad@ahmadh.com>
2023-01-02 22:14:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftProtohackers
+
+A description of this package.
---
+++ Sources/SwiftProtohackers/SwiftProtohackers.swift
@@ -0,0 +1,8 @@
+@main
+public struct SwiftProtohackers {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(SwiftProtohackers().text)
+    }
+}
